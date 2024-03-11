d4661d85029d6b15535ed18fb5c35589be9040cd
Daniel Ziegler <daniel.ziegler@senacor.com>
2024-03-11 11:22:02 +0100

    Initial commit

---
+++ src/test/kotlin/com/senacor/intermission/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.senacor.intermission.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
